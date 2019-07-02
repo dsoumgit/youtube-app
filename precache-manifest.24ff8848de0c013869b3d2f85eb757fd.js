e7a8ae0e0401fbf4780b992dab2ef8398feed953
Dara Soumgit <dara.soumgit@gmail.com>
2019-07-02 16:38:37 -0400

    Updates

---
+++ precache-manifest.24ff8848de0c013869b3d2f85eb757fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d3fd6a5899e221c2593867bf9b647e",
+    "url": "/youtube-app/index.html"
+  },
+  {
+    "revision": "9ff0567c5f85f7491ed2",
+    "url": "/youtube-app/static/css/main.fbf785d4.chunk.css"
+  },
+  {
+    "revision": "7e6ac1444ef9242ebc00",
+    "url": "/youtube-app/static/js/2.824d5390.chunk.js"
+  },
+  {
+    "revision": "9ff0567c5f85f7491ed2",
+    "url": "/youtube-app/static/js/main.25a7084b.chunk.js"
+  },
+  {
+    "revision": "bf759a3baa28ef32c904",
+    "url": "/youtube-app/static/js/runtime~main.b61650af.js"
+  }
+]);
